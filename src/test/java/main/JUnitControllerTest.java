fbe1028e6f462201349c85cfc59ec46bec122c09
Sergei <Serduyksb@gimai.com>
2022-01-17 14:42:10 +0300

    Test integration Application class running.

--- src/test/java/main/JUnitControllerTest.java
+++ src/test/java/main/JUnitControllerTest.java
@@ -1,22 +1,14 @@
 package main;
 
 import main.controller.ChatController;
-import main.model.Message;
-import main.model.User;
 import main.repository.MessageRepository;
 import main.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-
-import java.util.Date;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 /**
  * test
@@ -35,14 +27,5 @@ public class JUnitControllerTest {
     @Autowired
     private MessageRepository messageRepository;
 
-    @RequestMapping(value = "user/{id}", method = RequestMethod.GET)
-    public User get(@PathVariable String id) {
-        return userRepository.getBySessionId(id);
-    }
-
-    @RequestMapping(value = "message/{id}", method = RequestMethod.GET)
-    public Optional<Message> get(@PathVariable int id) {
-        return messageRepository.findById(id);
-    }
 
 }
